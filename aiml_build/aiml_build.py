ae80155099c0a0cbdbe6bb3742a72f0c1cbf5c49
Leonardo Sartori <leonardogt4@hotmail.com>
2019-03-25 13:19:16 -0300

    Add random question
    Add random question

--- aiml_build/aiml_build.py
+++ aiml_build/aiml_build.py
@@ -6,11 +6,9 @@
 #Contact: leonardogt4@hotmail.com
 
 '''
-#Sample AIML:
-
+#Sample AIML with normal question:
 <aiml version="1.0.1" encoding="UTF-8">
 <!-- basic_chat.aiml -->
-
     <category>
         <pattern>HELLO</pattern>
         <template>
@@ -26,6 +24,23 @@
     </category>
     
 </aiml>
+
+#Sample AIML with random question:
+<?xml version = "1.0" encoding = "UTF-8"?>
+<aiml version = "1.0.1" encoding ="UTF-8"?>
+   <category>
+      <pattern>HI</pattern>
+      
+      <template>
+         <random>
+            <li> Hello! </li>
+            <li> Hi! Nice to meet you! </li>
+         </random>
+      </template>
+      
+   <category>      
+</aiml>
+
 '''
 
 #def open_file():
@@ -56,6 +71,37 @@ def open_template_tag(responseMessage):
 
 def close_template_tag():
     file.write("\t\t</template>\n")
+
+def open_random_tag(listTextResponses):
+    file.write("\t\t<random>\n")
+    for i in len(listTextResponses):
+        file.write("\t\t\t<li>\n" + str(i) + "\t\t\t</li>\n")
+    
+
+def close_random_tag():
+    file.write("\t\t</random>\n")
+
+def add_question_default(textQuestion, textResponse):
+    open_aiml_tag()
+    open_category_tag()
+    open_pattern_tag(textQuestion)
+    close_pattern_tag()
+    open_template_tag(textResponse)
+    close_template_tag()
+    close_category_tag()
+    close_amil_tag()
+
+def add_question_random(textQuestion, listTextResponses):
+    open_aiml_tag()
+    open_category_tag()
+    open_pattern_tag(textQuestion)
+    close_pattern_tag()
+    open_random_tag();
+    close_random_tag(listTextResponses);
+    open_template_tag(textResponse)
+    close_template_tag()
+    close_category_tag()
+    close_amil_tag()
     
 def close_file():
     # Close opend file
@@ -63,11 +109,7 @@ def close_file():
 
 #Test
 #open_file()
-open_aiml_tag()
-open_category_tag()
-open_pattern_tag("My question")
-close_pattern_tag()
-open_template_tag("My response")
-close_template_tag()
-close_category_tag()
-close_amil_tag()
+
+add_question_default('Teste pergunta', 'Teste resposta')
+
+#TODO: Convert file.write commands in only one string with contacts
