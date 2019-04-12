0a44baf89084d107496446b021d30e00ef2bccbd
Leonardo Sartori <leonardogt4@hotmail.com>
2019-04-11 22:05:04 -0300

    Updated category tag
    Updated category tag

--- modules/aiml_build.py
+++ modules/aiml_build.py
@@ -12,31 +12,32 @@ sheet = spreadsheet()
 file = open("support.aiml", "w")
 
 def open_aiml_tag():
-    return ("<?xml version = \"1.0\" encoding=\"UTF-8\"?>"+"\n<aiml version=\"1.0.1\" encoding=\"UTF-8\">\n")
+    return ("<?xml version = \"1.0\" encoding=\"UTF-8\"?>"+"\n<aiml version=\"1.0\" encoding=\"UTF-8\">\n")
 
 def close_amil_tag():
     return "</aiml>"
 
 def write_pattern_tag(question_message):
-    return "\t\t<pattern>\n"+"\t\t\t"+question_message+"\n"+"\t\t</pattern>\n"
+    return "<pattern>\n"+question_message+"\n"+"</pattern>\n"
 
 def open_category_tag():
-    return "\t<category>\n"
+    return "<category>\n"
 
 def close_category_tag():
-    return "\t</category>\n"
+    return "</category>\n"
 
 def write_template_tag(responseMessage):
-    return "\t\t<template>\n"+"\t\t\t"+responseMessage+"\n"+"\t\t</template>\n"
+    return "<template>\n"+responseMessage+"\n"+"</template>\n"
 
 def add_questions_default(listQuestion, listResponse):
     text = ""
-    text += open_aiml_tag() + open_category_tag()
+    text += open_aiml_tag()
         
     listaPerguntasRespostas = list(zip(listQuestion[1:], listResponse[1:]))
     for line in listaPerguntasRespostas:
+        text += open_category_tag()
         text += write_pattern_tag(line[0]) + write_template_tag(line[1])
-    text += close_category_tag()
+        text += close_category_tag()
     text += close_amil_tag()
     file.write(text)
     file.close()
