fcd5ae95c33f2913f8396fe8c868ffba658e80d8
Leonardo Sartori <leonardogt4@hotmail.com>
2019-04-16 18:25:29 -0300

    Updated directory of client.json
    Update directory of client.json

--- modules/spreadsheets.py
+++ modules/spreadsheets.py
@@ -11,7 +11,7 @@ class spreadsheet():
 
     def __init__(self):
         scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-        creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+        creds = ServiceAccountCredentials.from_json_keyfile_name('../vars/client_secret.json', scope)
         client = gspread.authorize(creds)
         sheet = client.open('PlanilhaPerguntasRespostas').sheet1
         self.sheet=sheet
