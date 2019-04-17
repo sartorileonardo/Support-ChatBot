41828c660d20df6c792a07d77b91b2298fa27698
Leonardo Sartori <leonardogt4@hotmail.com>
2019-04-16 23:17:42 -0300

    Rename methods
    Rename methods

--- modules/aiml_read.py
+++ modules/aiml_read.py
@@ -1,26 +1,29 @@
 
+#Autor: Leonardo Sartori
+#Contact: leonardogt4@hotmail.com
+
 import aiml
 
-class AimlRead():
+class readAiml():
 
-    def __init__(self):
+    def __init__(self):	
         # The Kernel object is the public interface to
         # the AIML interpreter.
-        k = aiml.Kernel()
+        self.k = aiml.Kernel()
 
         # Use the 'learn' method to load the contents
         # of an AIML file into the Kernel.
-        k.learn("../vars/std-startup.xml")
+        self.k.learn("../vars/std-startup.xml")
 
         # Use the 'respond' method to compute the response
         # to a user's input string.  respond() returns
         # the interpreter's response, which in this case
         # we ignore.
-        k.respond("load aiml b")
-        self.k = k
-        return True
+        self.k.respond("load aiml b")
 
-    def response_aiml(self, text_message):
-        return self.k.respond(text_messase))
+    def response(self, message):
+        # Reading user input from the command
+        # line and printing responses.
 
-
+        # TODO: test with support.aiml, alter aiml repository and connect with flask web server 
+        print(self.k.respond(message))
