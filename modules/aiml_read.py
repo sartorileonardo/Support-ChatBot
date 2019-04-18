ed84f912bab0930464ba255665576996491e7354
andreiRibeiro <andreiribeiro.sc@gmail.com>
2019-04-18 10:00:39 -0300

    Exclusão arquivo
    Exclusão arquivo pois já exite na Master

--- modules/aiml_read.py
+++
@@ -1,9 +0,0 @@
-#Autor: Andrei A. Ribeiro
-import aiml
-import os
-
-def read_aiml():
-    os.chdir(os.getcwd()+'/vars')
-    ai = aiml.Kernel()
-    ai.learn('std-startup.xml')
-    ai.respond('load aiml b')
