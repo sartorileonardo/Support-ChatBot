91a5425ed24fe83c22a078ddbf1579e0f8d07ef8
Leonardo Sartori <leonardogt4@hotmail.com>
2019-05-21 13:57:17 -0300

    Added schedule to auto run
    Added schedule to auto run of aiml_build.py

---
+++ modules/schedule_periodic.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# -*- coding: latin-1 -*-
+
+#Autor: Leonardo Sartori
+#Contact: leonardogt4@hotmail.com
+
+import schedule  
+import time 
+from os import popen 
+
+def run_aiml_build():  
+    print("I'm working...")  
+    popen("python3 aiml_build.py")
+
+schedule.every(30).seconds.do(run_aiml_build)
+
+while True:  
+    schedule.run_pending()
+    time.sleep(1)
